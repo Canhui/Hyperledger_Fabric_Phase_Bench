0a4e3e4354ed9e07fe574772b2e5ef799bc0e3af
Canhui Wang, Ph.D. <320653941@qq.com>
2026-05-10 11:36:20 +0800

    Commit

---
+++ Exp8-fabric-samples/demo-first/workload/tls/prepare_tls_peer0_org2.sh
@@ -0,0 +1,49 @@
+# org info
+org_name="Org2"
+org_msp="Org2MSP"
+tls_file=`python -c 'import read_pem; print read_pem.read("org2")'`
+
+# peer info
+peer_name="peer0.org2.example.com"
+peer_addr="grpcs://peer0.org2.example.com:8051"
+file_name="connection-peer0-org2.json"
+
+# details
+cat>$HOME/fabric-samples/demo-first/workload/tls/$file_name<<EOF
+{
+    "name": "test-network-org1",
+    "version": "1.0.0",
+    "client": {
+        "organization": "$org_name",
+        "connection": {
+            "timeout": {
+                "peer": {
+                    "endorser": "300"
+                }
+            }
+        }
+    },
+    "organizations": {
+        "$org_name": {
+            "mspid": "$org_msp",
+            "peers": [
+                "$peer_name"
+            ]
+        }
+    },
+    "peers": {
+        "$peer_name": {
+            "url": "$peer_addr",
+            "tlsCACerts": {
+                "pem": "$tls_file"
+            },
+            "grpcOptions": {
+                "ssl-target-name-override": "$peer_name",
+                "hostnameOverride": "$peer_name"
+            }
+        }
+    }
+}
+EOF
+
+rm -rf *.pyc
